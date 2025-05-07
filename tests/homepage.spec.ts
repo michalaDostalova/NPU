aef519023b72e640436fffef35870793e3b3f1ca
Michala <michaladostalova@email.com>
2025-05-07 12:04:17 +0200

    homepage

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -3,5 +3,4 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://npu.cz/cs');
   await expect(page).toHaveTitle(/Národní památkový ústav/);
-
 })
